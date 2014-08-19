3044177fb1106e45d66ba1f56fa35abd1ebef158
Guilherme Baptista <guisgb13@gmail.com>
2014-08-19 21:54:49 +0200

    Forgot to include the sender info in user_data that is propagated

--- server.py
+++ server.py
@@ -210,6 +210,7 @@ def send_presence(username):
         return response
 
     user_data = {
+        "sender": username,
         "first_name": first_name,
         "full_name": full_name,
         "gender": gender,
