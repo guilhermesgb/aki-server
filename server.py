8242a8d8c15808cf57fcdb30a6d752b1f5389d9a
Guilherme Baptista <guisgb13@gmail.com>
2014-09-05 06:18:39 +0200

    Get members endpoint

--- server.py
+++ server.py
@@ -565,6 +565,21 @@ def send_exit():
     response.headers["Content-Type"] = "application/json"
     return response
 
+@server.route('/members', methods=['GET'])
+@login_required
+def get_members():
+
+    user_id = current_user.get_id()
+    chat_id = ChatRoom.at_chat(user_id)
+    if ( chat_id ):
+        members = ChatRoom.get_chat(chat_id).members
+        response = make_response(json.dumps({'server':'{} retrieved chat {} members list'.format(user_id, chat_id), 'code':'ok', 'members':members}), 200)
+    else:
+        response = make_response(json.dumps({'server':'{} is not in a chat room'.format(user_id), 'code':'error'}), 200)
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
 def do_send_message(sender, chat_room, message):
 
     database.session = database.create_scoped_session()
