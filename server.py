c50daf5d6cfacc8000fbd886cdecd27b2df7b869
Guilherme Baptista <guisgb13@gmail.com>
2014-12-05 10:29:18 +0100

    Maybe the array should be stringifyed...

--- server.py
+++ server.py
@@ -737,13 +737,10 @@ def do_send_message(sender, chat_ids, message):
         "action": "com.lespi.aki.receivers.INCOMING_MESSAGE",
     }
 
-    channels = []
-    for chat_id in chat_ids:
-        channels.append(chat_id)
     payload = {
         "where": {
             "inactive": True,
-            "channels": channels
+            "channels": str(chat_ids)
         },
         "data" : data
     }
